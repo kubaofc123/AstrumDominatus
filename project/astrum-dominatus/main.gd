2dff07c841ee900c681df1bc524ee5eab89e7b6e
kubaofc123 <kuba@2222.pl>
2026-02-12 11:31:56 +0100

    initial project setup

---
+++ project/astrum-dominatus/main.gd
@@ -0,0 +1,27 @@
+class_name Main
+extends Node
+
+## Part of [Node], main description ends here
+##
+## Long description goes here.
+## [br][br]
+## Uses [method Node._enter_tree]
+## [br][br]
+## @tutorial(Youtube Godot Docs Tutorial): https://youtu.be/B7G6sVNDczY
+## @tutorial(Godot Docs): https://docs.godotengine.org/en/stable/tutorials/scripting/gdscript/gdscript_documentation_comments.html
+
+#=============================== VARIABLES ===============================
+
+#================ PUBLIC ================
+
+#================ PRIVATE ================
+
+#=============================== FUNCTIONS ===============================
+
+#================ PUBLIC ================
+
+#================ PRIVATE ================
+
+#=============================== CALLBACKS ===============================
+
+########################## END OF FILE ##########################
