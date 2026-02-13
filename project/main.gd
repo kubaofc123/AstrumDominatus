308bf1d1d45a76210e9801c94798e9469d6ac11b
kubaofc123 <kuba@2222.pl>
2026-02-13 23:30:10 +0100

    added save validation, progress on main screen

--- project/main.gd
+++ project/main.gd
@@ -6,16 +6,18 @@ extends Node
 #================ PUBLIC ================
 
 @export_group("Setup")
-@export var mod_blacklist : PackedStringArray		## List of mods that if they appear in save file, app will not load the save
+@export var required_mods_list : PackedStringArray		## List of mods that need to be in the save file in order to load
+@export var mod_blacklist : PackedStringArray				## List of mods that if they appear in save file, app will not load the save
 @export_group("Setup/Backend")
-@export var backend_address : String				## Address of the backend where the results are being sent and status is received
-@export var backend_read_key : String			## Backend API key to receive status
+@export var backend_address : String						## Address of the backend where the results are being sent and status is received
+@export var backend_read_key : String					## Backend API key to receive status
 @export_group("Setup/Nodes")
 @export var state_container : Node = null
 
 var current_state : Main.EState = EState.INIT
 
 enum EState {INIT = 0, LOGIN = 1, MAIN = 2}
+enum ESaveFileValidity {VALID = 0, INVALID = 1, FILE_LOCKED = 2, BLACKLIST_MOD_PRESENT = 3, REQUIRED_MOD_MISSING = 4}
 
 #================ PRIVATE ================
 
@@ -72,6 +74,86 @@ func _resize_and_reposition_window() -> void:
 	get_viewport().get_window().size = __new_viewport_size
 	get_viewport().get_window().position = DisplayServer.screen_get_size(DisplayServer.get_primary_screen())/4.0 + Vector2(DisplayServer.screen_get_position(DisplayServer.get_primary_screen()))
 
+
+func _is_valid_save_file(p_path : String) -> ESaveFileValidity:
+	# Exit if empty path was provided
+	if p_path.is_empty():
+		return ESaveFileValidity.INVALID
+	
+	# Exit of couldn't open the save file
+	var __reader : ZIPReader = ZIPReader.new()
+	var _err : Error = __reader.open(p_path)
+	if _err != Error.OK:
+		return ESaveFileValidity.INVALID
+	
+	# Exit if "status" file doesn't exist
+	if not __reader.file_exists("status"):
+		return ESaveFileValidity.INVALID
+	
+	# Read the "status" file as bytes
+	var __bytes : PackedByteArray = __reader.read_file("status")
+	__reader.close()
+	
+	# Parse bytes into array of strings, where each line is one entry
+	var __file_string : String = __bytes.get_string_from_utf8()
+	var __string_arr : PackedStringArray = __file_string.split("\n")
+	
+	# Find save version
+	var __save_version : int = 0
+	for a in __string_arr:
+		if a.strip_edges().begins_with("{version"):
+			__save_version = int(a.remove_chars("{}").get_slice(" ",1))
+			break
+	
+	# Process file
+	match __save_version:
+		9:
+			# Find mods line indexes
+			var __mods_line_start_idx : int = 0
+			var __mods_line_end_idx : int = 0
+			for i in __string_arr.size():
+				# Search for mods start
+				if __mods_line_start_idx == 0:
+					if __string_arr[i].strip_edges().begins_with("{mods"):
+						__mods_line_start_idx = i + 1
+						continue
+				elif __mods_line_end_idx == 0:
+					if __string_arr[i].strip_edges() == "}":
+						__mods_line_end_idx = i
+						break
+			
+			if __mods_line_start_idx == 0 or __mods_line_end_idx == 0:
+				return ESaveFileValidity.INVALID
+			
+			# Create mod list
+			var __mods_string_arr : PackedStringArray = __string_arr.slice(__mods_line_start_idx, __mods_line_end_idx)
+			
+			# Sanitize mod list
+			for i in __mods_string_arr.size():
+				__mods_string_arr[i] = __mods_string_arr[i].get_slice(":", 0)
+				__mods_string_arr[i] = __mods_string_arr[i].remove_chars("\"")
+				__mods_string_arr[i] = __mods_string_arr[i].strip_edges()
+			
+			# Process blacklist
+			for a in mod_blacklist:
+				if __mods_string_arr.has(a):
+					return ESaveFileValidity.BLACKLIST_MOD_PRESENT
+			
+			# Process required mods list
+			var __all_required_mods_found : bool = true
+			for a in required_mods_list:
+				if not __mods_string_arr.has(a):
+					__all_required_mods_found = false
+					break
+			if not __all_required_mods_found:
+				return ESaveFileValidity.REQUIRED_MOD_MISSING
+			
+		_:
+			pass
+	
+	
+	return ESaveFileValidity.VALID
+	
 #=============================== CALLBACKS ===============================
 
 ########################## END OF FILE ##########################
--- project/states/state_main.gd
+++ project/states/state_main.gd
@@ -7,12 +7,20 @@ extends Node
 
 #================ PRIVATE ================
 
+@export_group("Setup")
+@export var color_standard : Color
+@export var color_bad : Color
+@export_group("Internal")
 @export var mission_popup_container : VBoxContainer = null
 @export var mission_popup_widget_class_uid : StringName
 @export var anim_player_save : AnimationPlayer = null
 @export var anim_player_status : AnimationPlayer = null
 @export var file_dialog : FileDialog = null
 @export var button_load_save_file : Button = null
+@export var label_save_file_status_value : Label = null
+@export var label_save_file_name_value : Label = null
+
+var file_status_label_shown : bool = false
 
 #=============================== FUNCTIONS ===============================
 
@@ -27,10 +35,13 @@ func _ready() -> void:
 	assert(anim_player_status)
 	assert(file_dialog)
 	assert(button_load_save_file)
+	assert(label_save_file_status_value)
+	assert(label_save_file_name_value)
 	
 	# Signals
 	anim_player_save.animation_finished.connect(_on_anim_player_save_animation_finished)
 	button_load_save_file.pressed.connect(_on_button_load_save_file_pressed)
+	file_dialog.file_selected.connect(_on_file_dialog_file_selected)
 	
 	# Clear mission popup container
 	for a in mission_popup_container.get_children():
@@ -40,13 +51,37 @@ func _ready() -> void:
 
 func _on_anim_player_save_animation_finished(p_anim_name : StringName) -> void:
 	match p_anim_name:
-		"a_save_file":
-			anim_player_save.play("a_save_file_status")
+		"a_save_file_status":
+			if not file_status_label_shown:
+				anim_player_save.play("a_save_file_status_value")
+				file_status_label_shown = true
 		_:
 			pass
 
 
 func _on_button_load_save_file_pressed() -> void:
 	file_dialog.visible = true
+
+
+func _on_file_dialog_file_selected(p_path : String) -> void:
+	var __result : Main.ESaveFileValidity = Global.main._is_valid_save_file(p_path)
+	
+	if anim_player_save.is_playing() and anim_player_save.current_animation == "a_save_file_status":
+		await anim_player_save.animation_finished
+		
+	anim_player_save.play("a_save_file_status_value")
+	file_status_label_shown = true
+	
+	label_save_file_name_value.text = p_path.get_file().get_basename()
+	
+	if __result == Main.ESaveFileValidity.VALID:
+		label_save_file_status_value.text = "Approved"
+		label_save_file_status_value.add_theme_color_override("font_color", color_standard)
+		label_save_file_name_value.add_theme_color_override("font_color", color_standard)
+	else:
+		label_save_file_status_value.text = "Rejected"
+		label_save_file_status_value.add_theme_color_override("font_color", color_bad)
+		label_save_file_name_value.add_theme_color_override("font_color", color_bad)
+		
 	
 ########################## END OF FILE ##########################
